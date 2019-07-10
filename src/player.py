efd7615dbafcac12a7721dce9a346d9b96aa7570
mrsteveson <patrick.steveson727@gmail.com>
2019-07-10 18:57:12 -0500

    included player methods

--- src/player.py
+++ src/player.py
@@ -3,10 +3,28 @@
 
 
 class Player:
-    def __init__(self, name, room, inventory=[]):
+    def __init__(self, name, room, items=[]):
         self.name = name
         self.room = room
-        self.inventory = inventory
+        self.items = items
 
     def __str__(self):
-        return f'Greetings, {self.name}, you are currently in {self.room}.'
+        return f'Greetings, {self.name}, you are currently in {self.room}'
+
+    def take(self, item):
+        for item in self.room.items:
+            if item.name == item:
+                self.items.append(item)
+                self.room.items.remove(item)
+                item.on_take()
+            else:
+                print(f'\n {item} is not in this room')
+
+    def drop(self, item):
+        for items in self.items:
+            if item.name == item:
+                self.items.remove(item)
+                self.room.items.append(item)
+                item.on_drop()
+            else:
+                print(f'\n {item} is not in your inventory')
