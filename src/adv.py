ac41a09b98ef7dc796db7606d83a873f72a94d8e
mrsteveson <patrick.steveson727@gmail.com>
2019-07-10 15:31:46 -0500

    able to input n/e/s/w/q in adv

--- src/adv.py
+++ src/adv.py
@@ -1,25 +1,34 @@
 from room import Room
+from player import Player
+from item import Item
+
 
 # Declare all the rooms
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     "North of you, the cave mount beckons", []),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", []),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", []),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""", []),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""", []),
+
+    'secret': Room("Secret Hallway", """A light breeze catches your attention,
+you find a small hidden passageway. Where might it lead?""", [])
+
 }
+
+    
 
 
 # Link rooms together
@@ -32,12 +41,15 @@ room['overlook'].s_to = room['foyer']
 room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
+room['treasure'].e_to = room['secret']
+room['secret'].w_to = room['treasure']
 
 #
 # Main
 #
 
 # Make a new player object that is currently in the 'outside' room.
+player = Player('Sir Patty', room['outside'], [])
 
 # Write a loop that:
 #
@@ -49,3 +61,26 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+print(f'\nWelcome to the Dungeon.\n')
+playermove = None
+
+while playermove != 'q':
+    print(f'{player.name}, is in the {player.room}')
+
+    print('\nChoose a direction:')
+    playermove = input('[n]orth, [e]ast, [s]outh, [w]est, [q]uit: ')
+
+    if playermove == 'n' and player.room.n_to:
+        player.room = player.room.n_to
+    elif playermove == 'e' and player.room.e_to:
+        player.room = player.room.e_to
+    elif playermove == 's' and player.room.s_to:
+        player.room = player.room.s_to
+    elif playermove == 'w' and player.room.w_to:
+        player.room = player.room.w_to
+    elif playermove == 'q':
+        print(f'\nThank you for playing, {player.name}! Exiting Game.')
+        break
+    else:
+        print('\nNo room located in that direction, please choose again.\n')
