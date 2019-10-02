e3b98136a133d99f2b91a02fdc42fc8a4d04834f
mrsteveson <patrick.steveson727@gmail.com>
2019-10-02 18:08:59 -0500

    command parser functional

--- src/adv.py
+++ src/adv.py
@@ -1,5 +1,7 @@
 from room import Room
-
+from player import Player
+from item import Item
+# Import class objects
 # Declare all the rooms
 
 room = {
@@ -49,3 +51,45 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+#------------------------------------------------------------------------------
+
+# Create player, start at the beginning, in the room labeled "outside".
+player = Player('Lord Grimm', room['outside'], [Item("Bow", "basic wooden bow")])
+
+# Welcome Message
+# Includes instructions for acceptable inputs,
+print('\n--------------------------------------------------------------------------------')
+print(f'\nWelcome to the Spooky Maze!\n')
+print(f'\nAvailable move direction commands: [n], [e], [s], [w]')
+# print(f'Available item commands: get, take, drop, remove')
+print(f'Press [q] to quit the program')
+print('\n--------------------------------------------------------------------------------')
+
+# Print player.name & player.room
+def current_location(player):
+    print(f"\n\n\n{player.name}, is currently in the {player.room}. Do you wish to continue?")
+    print('\n-----------------------------------------------------------------------------')
+
+# Create an array using the four moveable directions. 
+directions = ['n', 'e', 's', 'w']
+
+# Create a control loop to handle the game.
+# Using while true will allow it to run until closed out.
+while True:
+    # Print out player information as player moves through the game
+    current_location(player)
+    # Print out available command options
+    print('\nChoose a direction: [n]orth, [e]ast, [s]outh, [w]est')
+    print('Press [q] to quit the program')
+    user_input = input(">>>>> ").lower()  # use .lower to ensure inputs are brought into a useable format
+    print('\n--------------------------------------------------------------------------------')
+    new_input = user_input.split(" ")  # Split at the space allowing for more than just cardinal direction inputs.
+    # Input controller
+    if len(new_input) == 1:
+        if user_input[0] in directions:  # Check if the input is valid
+            player.move(user_input)
+        elif user_input[0] == 'q':
+            print(f"\n\nCome back soon, {player.name}. Hope you enjoyed your stay. Exiting game now.")
+            break
+        else:
+            print('\nInvalid input, please choose from the available commands.')
