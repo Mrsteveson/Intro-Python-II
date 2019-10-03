a71c1ed6f6e6bf101354f6783fcd0d53667ec44a
mrsteveson <patrick.steveson727@gmail.com>
2019-10-03 15:23:29 -0500

    created pickup drop and view functions

--- src/player.py
+++ src/player.py
@@ -12,9 +12,28 @@ class Player:
 
     def __str__(self):
         return f"Greetings, {self.name}, you are currently in {self.room}."
-
+    # Function to move player throughout maze
     def move(self, direction):
         if getattr(self.room, f"{direction}_to") is not None:
             self.room = getattr(self.room, f"{direction}_to")
         else:
             print("\nThere is no room in that direction, please choose again.")
+    # Function to pickup item and add to inventory
+    def pickup(self, item):
+        if item in self.room.items:
+            self.items.append(item)
+            self.room.items.remove(item)
+            item.on_pickup()
+        else:
+            print(f'\n{item} does not exist in this room.\n')
+    # Function to drop item from inventory and add to the room
+    def drop(self, item):
+        if item in self.items:
+            self.items.remove(item)
+            self.room.items.append(item)
+            item.on_drop()
+        else:
+            print(f'\nYou do not possess {item}.\n')
+
+    def view_inventory(self):
+        print(f"\n\n{self.name}'s possessions:\n" + "," .join([item.name for item in self.items]))
