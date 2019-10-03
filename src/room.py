a75dd28847979fbd7e5040ca3960c3ae2401f0b7
mrsteveson <patrick.steveson727@gmail.com>
2019-10-03 15:23:53 -0500

    added item argument to room class

--- src/room.py
+++ src/room.py
@@ -2,13 +2,13 @@
 # description attributes.
 
 class Room:
-    def __init__(self, name, description):
+    def __init__(self, name, description, items):
         self.name = name
         self.description = description
-        # if items == None:
-        #     self.items = []
-        # else:
-        #     self.items = items
+        if items == None:
+            self.items = []
+        else:
+            self.items = items
 
         self.n_to = None
         self.s_to = None
